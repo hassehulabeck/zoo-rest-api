57dd7d0a2b368cb95f1af13d268e53dc12393c7a
hassehulabeck <hansbudandersson@gmail.com>
2020-03-15 17:56:32 +0100

    Finally fixed stupid async/await

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const router = require('./routes.js')
 const bodyParser = require('body-parser')
-const mysql = require('mysql')
+const mysql = require('mysql2')
+
+/* Mysql2 har inbyggt stöd för promises och async/await, vilket mysql saknar (märkligt nog) */
 
 let connection = mysql.createConnection({
     host: 'localhost',
@@ -27,26 +29,31 @@ app.use('/', async (req, res, next) => {
     // Starta kontakt med servern.
     connection.connect()
 
-    let sql = "SELECT COUNT(id) AS antal, role FROM users WHERE apikey = " + connection.escape(path)
+    let sql = "SELECT COUNT(id) AS antal, role FROM users WHERE apikey = ?"
 
-    await new Promise((resolve, reject) => {
-
-        connection.query(sql, (err, result, field) => {
-            if (err) throw err
-            if (result[0].antal == 1) {
-                req.body.hasAccess = true
-                req.body.accessRole = result[0].role
-                console.log(req.body)
-                resolve(req.body)
-                /* 
-                hasAccess är om api-key i URIen är betrodd, och accessRole är vilka rättigheter api-key ger. 
-                */
-            }
-        })
-        connection.end()
+    /* Gör själva query-funktionen async för att kunna vänta in resultatet från db.*/
+    let resultat = await connection.promise().query(sql, [path], (err, result, fields) => {
+        if (err)
+            throw err
     })
+
+    /* Behövde laborera ett tag för att hitta rätt i resultatet */
+    console.log(resultat[0][0].antal)
+
+    /* Om vi får rätt svar från db:n, så sätter vi några egenskaper i req.body så att resten av applikationen kan se om URI-anropet har en betrodd API-key */
+    if (resultat[0][0].antal == 1) {
+        console.log("Hurra")
+        req.body.hasAccess = true
+        req.body.accessRole = resultat[0][0].role
+        /*
+        hasAccess är om api-key i URIen är betrodd, och accessRole är vilka rättigheter api-key ger, dvs authentication resp authorization.
+        */
+    }
+
+    /* Kör man utan async/await, så hinner next köra innan db-queryn är klar. */
     next()
 })
+
 app.use('/', router)
 
 app.listen(8080, () => {
